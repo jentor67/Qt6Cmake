dfd13d78eca87d4b5c487a0059f8f25ce95b8dc0
jentor67 <john.jentor@gmail.com>
2025-07-21 23:39:49 -0500

    Add CPU-Usage project

---
+++ CPU-Usage/mainwindow.cpp
@@ -0,0 +1,60 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "QDebug"
+#include "QProcess"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    QString command;
+    // Get the number of CPU's
+    command = "lscpu | grep \"^CPU(s):\" | awk '{print $2}'";
+    qDebug() << processBash(command);
+
+    command = "echo 100 - $(mpstat -P 0 | tail -1 | awk \'{print $13}\') | bc";
+    qDebug() << processBash(command);
+
+    command = "echo 100 - $(mpstat -P 1 | tail -1 | awk \'{print $13}\') | bc";
+    qDebug()  << processBash(command);
+}
+
+QString MainWindow::processBash(QString command) {
+
+
+    QProcess process;
+    QString program = "/usr/bin/bash"; // Or "/usr/bin/bash" depending on your system
+
+    QStringList arguments;
+    arguments << "-c" <<command;
+    process.start(program,arguments);
+
+    // Optional: Wait for the process to finish
+    if (!process.waitForFinished()) {
+        qDebug() << "Process failed to finish:" << process.errorString();
+    }
+
+    // // Optional: Read standard output
+    QString output = process.readAllStandardOutput();
+    // qDebug() << "Standard Output:\n" << output;
+
+    // // Optional: Read standard error
+    // QByteArray error = process.readAllStandardError();
+    // qDebug() << "Standard Error:\n" << error;
+
+    return output;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
